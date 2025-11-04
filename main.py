8d3d28c95864b6e5e3091e29c9666bfe179cc59b
Asad <syedasadrazarizvi1@gmail.com>
2025-11-04 12:12:01 +1100

    Minor change, but use fgo_pipeline.py instead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from propagator import OrbitPropagator, plot_orbit_3d
 
 prop = OrbitPropagator("orbDetHOUSE")
-csv = prop.propagate("configs/config_geo_realistic.yml", output_file="geo_truth.csv")
-plot_orbit_3d(csv, output_file="orbit.png")
+csv = prop.propagate("configs/config_geo_realistic.yml", output_file="out/geo_truth.csv")
+plot_orbit_3d(csv, output_file="plots/orbit.png")
